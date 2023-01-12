3e64f5298b5f775e05dd4f4608a6f341b964cea4
jonbassi <jonbassi64@outlook.com>
2023-01-12 14:05:05 -0300

    new function in functionGetMove.js

--- api/functionGETmove.js
+++ api/functionGETmove.js
@@ -1,62 +1,92 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = {
-    async moveInformations(move) {
-        try {
-            let url = `https://pokeapi.co/api/v2/move/${move}`
+  async moveInformations(move) {
+    //    try {
+    let url = `https://pokeapi.co/api/v2/move/${move}`;
 
-            const response = await axios.get(url)
+    const response = await axios.get(url);
+    let data = response.data;
 
-            let data = (response.data);
-            return this.getMove(data);
+    return this.getMove(data);
+    // }
+    // catch (err) {
+    //    return err.response.data
+    // }
+  },
+
+  getMove(move) {
+    let accuracy = move.accuracy;
+    let dmgClass = move.damage_class.name;
+    let percent = String(move.effect_chance) + " %";
+    let listOfPokes = move.learned_by_pokemon;
+    let name = move.name;
+    let power = move.power;
+    let pp = move.pp;
+    let target = move.target.name;
+    let moveType = move.type.name;
+
+    let seed = 0;
+    let pokes = [];
+    let listMax = 3;
+
+    listOfPokes = this.filterPokeName(listOfPokes);
+
+    if (listMax.length < 3) listMax = listOfPokes.length;
+
+    for (let i = 0; i < 3; i++) {
+      seed = Math.round(Math.random() * listOfPokes.length);
+      let duplicated = false;
+
+      if (seed == listOfPokes.length) seed--;
+
+      let name = listOfPokes[seed].name;
+
+      name = name.charAt(0).toUpperCase() + name.slice(1);
+
+      pokes.forEach((item) => {
+        if (item == name) {
+          duplicated = true;
+          i--;
         }
-        catch (err) {
-            return err.response.data
-        }
-    },
+      });
 
-    getMove(move) {
-
-        let accuracy = move.accuracy;
-        let dmgClass = move.damage_class.name;
-        let percent = String(move.effect_chance) + " %";
-        let listOfPokes = move.learned_by_pokemon;
-        let name = move.name;
-        let power = move.power;
-        let pp = move.pp;
-        let target = move.target.name;
-        let moveType = move.type.name;
-
-        let seed = 0;
-        let pokes = [];
-
-        for (let i = 0; i < 3; i++) {
-            seed = Math.round(Math.random() * listOfPokes.length);
-            let name = listOfPokes[seed].name;
-
-            if (name.includes('pikachu-')) {
-                i--;
-                break;
-            }
-            name = name.charAt(0).toUpperCase()
-                + name.slice(1)
-            pokes.push(name);
-        }
-
-        let txT = `${pokes[0]}, ${pokes[1]} e ${pokes[2]} podem aprender esse golpe.`;
-
-        informations = {
-            "accuracy": accuracy,
-            "dmgClass": dmgClass,
-            "percent": percent,
-            "name": name,
-            "power": power,
-            "pp": pp,
-            "target": target,
-            "moveType": moveType,
-            "pokesWhoCanLearn": txT,
-        }
-
-        return informations;
+      if (!duplicated) pokes.push(name);
     }
-}
+
+    informations = {
+      accuracy: accuracy,
+      dmgClass: dmgClass,
+      percent: percent,
+      name: name,
+      power: power,
+      pp: pp,
+      target: target,
+      moveType: moveType,
+      pokesWhoCanLearn: pokes,
+    };
+
+    return informations;
+  },
+
+  // função para bloquear alguns pokemons
+
+  filterPokeName(array) {
+    for (let i = 0; i < array.length; i++) {
+      if (
+        array[i].name.includes("pikachu-") ||
+        array[i].name.includes("-gmax")    ||
+        array[i].name.includes("-mega")    ||
+        array[i].name.includes("-aria")    ||
+        array[i].name.includes("-hangry")  ||
+        array[i].name.includes("-primal")  ||
+        array[i].name.includes("-totem")   ||
+        array[i].name.includes("-busted")
+      ) {
+        array.splice(i, 1);
+        i--;
+      }
+    }
+    return array;
+  },
+};
--- api/mockado.js
+++
@@ -1,9 +0,0 @@
-let held_by_pokemon = [];
-
-let listOfPokes = [];
-
-held_by_pokemon.forEach(item => {
-    listOfPokes.push(item.pokemon.name);
-});
-
-console.log(listOfPokes)
